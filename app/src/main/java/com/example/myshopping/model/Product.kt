bb7f08c408c9508648e2f63c275bbb0a765b1ac4
Adhish Mathur <adhish.mathur@gmail.com>
2022-05-11 17:35:48 +0530

    Setup dependencies and added POJO model

---
+++ app/src/main/java/com/example/myshopping/model/Product.kt
@@ -0,0 +1,8 @@
+package com.example.myshopping.model
+
+data class Product(
+    val image_url: String?,
+    val name: String?,
+    val price: String?,
+    val rating: Int?
+)
---
+++ app/src/main/java/com/example/myshopping/model/ProductEntity.kt
@@ -0,0 +1,5 @@
+package com.example.myshopping.model
+
+data class ProductEntity(
+    val products: List<Product>?
+)
